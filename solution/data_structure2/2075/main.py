e56aba83123d606ab27546cd00b44e6242cc61c7
kwakseongjae <gkffhdnls13@gmail.com>
2024-01-16 13:41:21 +0900

    refactor: 2075 python solution

--- solution/data_structure2/2075/main.py
+++ solution/data_structure2/2075/main.py
@@ -13,9 +13,11 @@ heap = []
 for _ in range(N):
     numbers = list(map(int, input().split()))
     for number in numbers:
-        if len(heap) < N: # heap의 크기를 N개로 유지
+        # heap의 크기를 N개로 유지
+        if len(heap) < N:
                 heapq.heappush(heap, number)
-        else:
+        # 우선순위 큐의 최솟값을 제거하고 현재 확인하고 있는 숫자를 우선순위 큐에 삽입    
+        else: 
             if heap[0] < number:
                 print(heapq.heappop(heap))
                 heapq.heappush(heap, number)
